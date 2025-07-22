e072a9d29bea782f2c6eff6d72be6e027ae50cf1
gunysunny <mikan31@naver.com>
2025-07-22 15:56:52 +0900

    refactor(component): PlaceListFrame에서 Card와 placeAPI.js 함수로 구조 개선

--- src/components/PlaceListFrame.jsx
+++ src/components/PlaceListFrame.jsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import usePlaces from "../api/usePlaces";
+import usePlaces, { fetchPlaces } from "../api/placeAPI";
+import PlaceCard from "./Card";
+
 
 const baseUrl = "http://localhost:3000"; // 백엔드 서버 주소
 
 export default function PlaceListFrame() {
-    const { places, loading, error } = usePlaces();
+    const [places, setPlaces] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        fetchPlaces() 
+            .then(setPlaces) // fetchplace가 성공적으로 불러와지면 then이 실행됨.
+            .catch(setError)
+            .finally(() => setLoading(false));
+    }, []);
 
     if (loading) return <div>로딩중...</div>;
     if (error) return <div>에러 발생!</div>;
@@ -15,18 +27,7 @@ export default function PlaceListFrame() {
             <h2 className="text-center text-lg font-semibold mb-6">맛집 목록</h2>
             <div className="grid grid-cols-4 gap-8">
                 {places.map((place) => (
-                <div
-                    key={place.id}
-                    className="w-64 h-80 bg-gray-100 rounded-2xl flex flex-col items-center justify-start p-4 shadow-md cursor-pointer hover:bg-blue-100 transition"
-                >
-                    <img
-                        src={`${baseUrl}/${place.image.src}`}
-                        alt={place.image.alt}
-                        className="w-full h-44 object-cover rounded-xl"
-                    />
-                    <div className="mt-3 text-base font-bold">{place.title}</div>
-                    <div className="text-xs text-gray-500 text-center mt-1">{place.description}</div>
-                </div>
+                <PlaceCard key={place.id} place={place} />
                 ))}
             </div>
         </div>
